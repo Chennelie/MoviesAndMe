f384e86ba41b4c5eaa25b487aadb1fe18df2f995
Chennelie <laetitia.huriaux@gmail.com>
2020-02-20 18:41:37 +0100

    Revert "Implementing the native-base library"
    This reverts commit b13a989adf919417f9088365ad96cbe51a9730cd.

--- Components/Search.js
+++ Components/Search.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { AppLoading } from 'expo'
-import * as Font from 'expo-font'
-import { StyleSheet, View, FlatList, ActivityIndicator } from 'react-native'
-import { Container, Content, Form, Item, Label, Input, Button, Text } from 'native-base'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
@@ -15,18 +12,8 @@ class Search extends React.Component {
     this.totalPages = 0
     this.state = {
       films: [],
-      isLoading: false,
-      isReady: false
+      isLoading: false
     }
-  }
-
-  //Load the fonts to use the Text balises in nativebase
-  async componentDidMount() {
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
-    });
-    this.setState({ isReady: true });
   }
 
   _loadFilms() {
@@ -60,50 +47,53 @@ class Search extends React.Component {
   _displayLoading() {
     if (this.state.isLoading) {
       return (
-        <Container style={styles.loading_container}>
+        <View style={styles.loading_container}>
           <ActivityIndicator size='large' />
-        </Container>
+        </View>
       )
     }
   }
 
   render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
     return (
-      <Container style={styles.main_container}>
-        <Content padder>
-          <Form>
-            <Item floatingLabel>
-              <Label>Titre du film</Label>
-              <Input
-                style={styles.textinput}
-                onChangeText={(text) => this._searchTextInputChanged(text)}
-                onSubmitEditing={() => this._searchFilms()}
-              />
-            </Item>
-            <Button primary onPress={() => this._searchFilms()}><Text>Rechercher</Text></Button>
-          </Form>
-          <FlatList
-            data={this.state.films}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({item}) => <FilmItem film={item}/>}
-            onEndReachedThreshold={0.5}
-            onEndReached={() => {
-                if (this.page < this.totalPages) {
-                   this._loadFilms()
-                }
-            }}
-          />
-          {this._displayLoading()}
-        </Content>
-      </Container>
+      <View style={styles.main_container}>
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._searchFilms()}
+        />
+        <Button title='Rechercher' onPress={() => this._searchFilms()}/>
+        <FlatList
+          data={this.state.films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem film={item}/>}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+              if (this.page < this.totalPages) {
+                 this._loadFilms()
+              }
+          }}
+        />
+        {this._displayLoading()}
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  },
   loading_container: {
     position: 'absolute',
     left: 0,
