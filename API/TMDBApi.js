c143abf1401a44dfdd78c719806089083d2b5594
Chennelie <laetitia.huriaux@gmail.com>
2020-02-22 19:15:04 +0100

    Get and show he details of a movie

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,5 +1,6 @@
 const API_TOKEN = "657cb157a7fd6debbacfbe852148213d";
 
+//Get a movie list by a keyword
 export function getFilmsFromApiWithSearchedText (text, page) {
   const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
   return fetch(url)
@@ -7,6 +8,14 @@ export function getFilmsFromApiWithSearchedText (text, page) {
   .catch((error) => console.error(error))
 }
 
+//Get a movie image
 export function getImageFromApi (name) {
   return 'https://image.tmdb.org/t/p/w300' + name
+}
+
+//Get a movie details
+export function getFilmDetailFromApi (id) {
+  return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
 }
